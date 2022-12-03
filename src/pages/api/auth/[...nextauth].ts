c98b704fab3a2b3303ff7abafc2cae516fe01b93
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-12-03 15:46:53 +1100

    test deploy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "../../../env/server.mjs";
 
@@ -15,10 +14,6 @@ export const authOptions: NextAuthOptions = {
   },
   // Configure one or more authentication providers
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     // ...add more providers here
   ],
 };

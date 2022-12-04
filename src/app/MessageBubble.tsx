12c1018690a0fef01585a096caa9bfb2c6f282a0
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-12-04 14:03:04 +1100

    left or right chat bubble

--- src/app/MessageBubble.tsx
+++ src/app/MessageBubble.tsx
@@ -1,24 +1,40 @@
 import { type Message } from '$types';
+import clsx from 'clsx';
 import Image from 'next/image';
 
 type MessageBubbleProps = { message: Message };
 
+const isMe = true;
+
 export default function MessageBubble({ message: m }: MessageBubbleProps) {
   return (
-    <div className="flex w-fit">
-      <div className="flex-shrink-0">
+    <div className={clsx('flex w-fit gap-2', isMe && 'ml-auto')}>
+      <div className={clsx('grid flex-shrink-0 place-items-center', isMe && 'order-2')}>
         <Image src={m.profilePic} height={10} width={50} alt="Profile picture" />
       </div>
 
       <div>
-        <p className="px-1 pb-1 text-xs">{m.username}</p>
+        <p
+          className={clsx(
+            'px-1 pb-1 text-xs',
+            isMe ? 'text-right text-blue-400' : 'text-left text-red-400'
+          )}
+        >
+          {m.username}
+        </p>
 
         <div className="flex items-center">
-          <div className="w-fit rounded-lg bg-red-300 px-3 py-2 text-white">
+          <div
+            className={clsx(
+              'w-fit rounded-lg px-3 py-2 text-white',
+              isMe ? 'order-2 bg-blue-400' : 'bg-red-400'
+            )}
+          >
             <p className="">{m.message}</p>
           </div>
-          <p className="px-2 text-xs italic text-gray-400">
-            {/* {new Date(m.createdAt).toLocaleString()} */}
+
+          <p className={clsx('px-2 text-xs italic text-gray-400', isMe && 'text-right')}>
+            {new Date(m.createdAt).toLocaleString()}
           </p>
         </div>
       </div>

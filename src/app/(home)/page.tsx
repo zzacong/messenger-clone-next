ada8fadb42abd5c96e1a0bbf640a2d7458fc986f
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-12-04 18:54:30 +1100

    with middleware auth redirect

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,9 @@
 import { getMessages } from '$server/common/get-messages';
-import { getServerAuthSession } from '$server/common/get-server-auth-session';
-import { redirect } from 'next/navigation';
 
 import ChatInput from './ChatInput';
 import MessageList from './MessageList';
 
 export default async function HomePage() {
-  const session = await getServerAuthSession();
-  if (!session) redirect('/auth/signin');
-
   const messages = await getMessages();
   // mock loading UI
   await new Promise(res => setTimeout(res, 2000));

6f6ca26724f9b0eea87000fe896cfebcbcf35972
zzacong <61817066+zzacong@users.noreply.github.com>
2022-12-05 14:32:07 +1100

    pusher instances manage

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,7 +1,9 @@
 import Pusher from 'pusher-js';
 import { env } from '$env/client.mjs';
 
-export const pusherClient = new Pusher(env.NEXT_PUBLIC_PUSHER_KEY, {
-  cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
-  authEndpoint: '/api/pusher/auth-channel',
-});
+export const pusherClient = Pusher.instances.length
+  ? (Pusher.instances[0] as Pusher)
+  : new Pusher(env.NEXT_PUBLIC_PUSHER_KEY, {
+      cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
+      authEndpoint: '/api/pusher/auth-channel',
+    });
